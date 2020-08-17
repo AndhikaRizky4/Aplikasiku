968dc7c9657acdb9991346516ead9029e1ed9669
AndhikaRizky4 <dhika.10rpl.2019@gmail.com>
2020-08-17 09:38:19 +0700

    Add SP

--- app/src/main/java/com/example/aplikasiku/controlclaass.java
+++ app/src/main/java/com/example/aplikasiku/controlclaass.java
@@ -2,13 +2,27 @@ package com.example.aplikasiku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class controlclaass extends AppCompatActivity {
 
+    private SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_controlclaass);
+        sharedPreferences = getSharedPreferences("Login", Context.MODE_PRIVATE);
+        if (!sharedPreferences.getString("username", "").isEmpty() ||
+                sharedPreferences.getString("username", "").equalsIgnoreCase("")) {
+            //logindulu
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }else {
+            startActivity(new Intent(getApplicationContext(),MainMenu.class));
+            finish();
+        }
     }
 }
